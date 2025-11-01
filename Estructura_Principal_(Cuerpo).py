c7330e93c37aaf89203f226f2b3372ba0003ffb3
infor09 <informatorio9@gmail.com>
2025-11-01 19:06:57 -0300

    Agrego un Menú, pero que es parte del la aplicación, no da contenido pero sirve para "configurar", o algo así

--- Estructura_Principal_(Cuerpo).py
+++ Estructura_Principal_(Cuerpo).py
@@ -1,38 +1,50 @@
 import tkinter as tk
+from tkinter import messagebox
 
-def saludar():
-    entrada2 = entrada.get()
-    etiqueta.config(text= f"hola, {entrada2}!" )
     
 ventana = tk.Tk()
 ventana.title("Programa")
-ventana.geometry("300x200") 
+#ventana.geometry("300x200") 
 
 
 ###Editar Ventana###
 ventana.configure(
-    bg="#494949",  # Color de fondo
+    bg="#494949",  # Color de fondo de la ventana
     #lo mismo de geometry que está arriba:
     width=800,       # Ancho de la ventana
     height=600,      # Alto de la ventana
 )
-# Permitir redimensionar la ventana
+# El usuario puede redimensionar la ventana con esto:
 ventana.resizable(True, True)  # (ancho, alto)
 
-###Editar Etiqueta###
+###Menu del Programa en sí###
+
+menu_horizontal = tk.Menu(ventana) #Crea el menú
+ventana.config(menu=menu_horizontal) #se lo asocia a la instancia de la ventana
+
+###Esto es para crear los Menús###
+# El teroff=1 es para que se separen las opciones de la ventana, como una miniventana, caso contrario, tearoff=0, se queda donde está
+menu_ejemplo = tk.Menu(menu_horizontal, tearoff=0)
+menu_ejemplo.add_command(label="Menu 1", command=lambda: messagebox.showinfo("Título 1", "Opción 1"))
+menu_ejemplo.add_command(label="Menu 2", command=lambda: messagebox.showinfo("Título 2", "Opción 2"))
+menu_ejemplo.add_command(label="Menu 3", command=lambda: messagebox.showinfo("Título 3", "Opción 3"))
+menu_ejemplo.add_command(label="Menu 4", command=lambda: messagebox.showinfo("Título 4", "Opción 4"))
+menu_ejemplo.add_command(label="Menu 5", command=lambda: messagebox.showinfo("Título 5", "Opción 5"))
 
 
+# Agregar el menu_ejemplo a la barra de menús
+menu_horizontal.add_cascade(label="Archivo", menu=menu_ejemplo) #El add_cascade() sirve para agregar submenú a un menú principal
 
-etiqueta= tk.Label(ventana,text="Holaaa")
-etiqueta.pack()
+# Hace lo mismo que lo anterior
+menu_ejemplo2 = tk.Menu(menu_horizontal, tearoff=0)
+menu_ejemplo2.add_command(label="Acerca de", command=lambda: messagebox.showinfo("Programa TPI", "Versión 1.0"))
 
-entrada = tk.Entry(ventana)
-entrada.pack()
+# Agregar el menú 'Ayuda' a la barra de menús
+menu_horizontal.add_cascade(label="Ayuda", menu=menu_ejemplo2)
 
-boton = tk.Button(ventana, text = "Saludar", command = saludar)
-boton.pack()
-
+#Esto inicia la ventana y la va actualizando todo el rato
 ventana.mainloop()
+
 
 
 
